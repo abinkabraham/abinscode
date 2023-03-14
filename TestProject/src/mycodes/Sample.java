775acfd3ff145715652a009dd90c838f99cf08a1
Abin <Abin@DESKTOP-HM9N3U8>
2023-03-14 10:46:28 +0530

    new data from eclipse

--- TestProject/src/mycodes/Sample.java
+++ TestProject/src/mycodes/Sample.java
@@ -4,5 +4,6 @@ public class Sample {
 public static void main(String[] args) {
 	System.out.println("Hello World");
 	System.out.println("Changes made in GIT");
+	System.out.println("new data from eclipse");
 }
 }
